effcd2b8e70c0cad1239f3224c44f19f32c9202f
Taylor Hunt <62562348+Airamentis@users.noreply.github.com>
2023-11-23 17:28:38 -0700

    refactored pubspec added player image added game components

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget(game: SpaceShooterGame()));
+}
+
+class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    sprite = await gameRef.loadSprite('player-sprite.png');
+
+    position = gameRef.size / 2;
+    width = 50;
+    height = 75;
+    anchor = Anchor.center;
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
+}
+
+class SpaceShooterGame extends FlameGame with PanDetector {
+  late Player player;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    player = Player();
+
+    add(player);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    player.move(info.delta.global);
+  }
 }
